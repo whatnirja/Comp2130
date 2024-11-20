dcb7d1bc4ed8950125d473884201a94f180a70fe
Nirja <127688817+whatnirja@users.noreply.github.com>
2024-11-20 14:54:12 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+        
+    @Override
+        public void start(Stage stage) throws Exception {
+            
+        }
+}
